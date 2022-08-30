ab7693867842a025c7ff2f588481c8b68d425a07
ndakalajulius <julius.ndakala@student.moringaschool>
2022-08-30 15:48:46 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,20 @@
+scooter= function(year,color, model){
+
+    this.year = year;
+    this.color= color
+    this.model = model;
+
+}
+driver= function(name,age, experience){
+
+    this.name = name;
+    this.age= age;
+    this.experience= experience;
+
+}
+PickupLocation= function(city, address){
+    this.city = city;
+    this.address= address;
+
+}
+
